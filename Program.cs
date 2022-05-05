94a26dec821694e396c9983c159b0b87b795a4c1
FrankA94 <96749300+FrankA94@users.noreply.github.com>
2022-05-05 14:21:46 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Variabeltilordning
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            string stringVar = "halla";
+            char charVar = 'A';
+            int a = 3;
+            float floatVar = 10.2f;
+            long longVar = 10000L;
+            decimal b = 3.0m;
+            double doubleVar = 10;
+            bool boolVar = true;
+            decimal sum = a + b;
+
+            Console.WriteLine(a+b);
+            Console.WriteLine(sum);
+        }
+    }
+}
